cf581e914cceda028f631f110ee0aaf544ea3311
Pavan Kumar Anumoju <pavananumoju@gmail.com>
2026-05-16 19:08:10 +0530

    Remove legacy inspection-repo codebase

--- inspection-repo/components/utils/mock-utils.js
+++
@@ -1,4 +0,0 @@
-import { getDate_wwwddmmyyyy } from "./date-utils";
-
-export const todaysMatchDate = new Date(2023, 3, 2);
-export const currentMatchDay = getDate_wwwddmmyyyy(todaysMatchDate);
--- inspection-repo/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ['cricbuzz-cricket.p.rapidapi.com'],
-  },
-}
-
-module.exports = nextConfig
